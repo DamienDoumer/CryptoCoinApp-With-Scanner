ace79d3fefc07194d4306c45788981a9f57a931b
Damien <damientohin@gmail.com>
2018-03-20 00:13:43 +0100

    I added basic QR scanning code

---
+++ CryptoCoinMon/CryptoCoinMon/ViewModels/QRViewModel.cs
@@ -0,0 +1,48 @@
+﻿using CryptoCoinMon.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ZXing.Net.Mobile.Forms;
+
+namespace CryptoCoinMon.ViewModels
+{
+    public class QRViewModel : BindableBase
+    {
+        private ZXingScannerPage _scannerPage;
+        private bool _isScanning;
+        public bool IsScanning
+        {
+            get => _isScanning; 
+            set =>SetProperty(ref _isScanning, value);
+        }
+
+        private string _scannedCode;
+        public string ScannedCode
+        {
+            get => _scannedCode;
+            set => SetProperty(ref _scannedCode, value);
+        }
+
+
+        public RelayCommand ScannQR { get; private set; }
+
+        public QRViewModel()
+        {
+            ScannQR = new RelayCommand(async () =>
+            {
+                var expectedFormat = ZXing.BarcodeFormat.QR_CODE;
+                var opts = new ZXing.Mobile.MobileBarcodeScanningOptions
+                {
+                    PossibleFormats = new List<ZXing.BarcodeFormat> { expectedFormat }
+                };
+
+                _scannerPage = new ZXingScannerPage(opts);
+
+                _scannerPage.OnScanResult += (result) =>
+                {
+
+                };
+            });
+        }
+    }
+}
