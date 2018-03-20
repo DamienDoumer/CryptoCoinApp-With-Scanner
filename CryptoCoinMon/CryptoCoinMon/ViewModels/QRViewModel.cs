129c4271c787f4aad93b74bd8bcffb779f9ce727
Damien <damientohin@gmail.com>
2018-03-20 01:49:07 +0100

    I added removal of QR screen after scanning

--- CryptoCoinMon/CryptoCoinMon/ViewModels/QRViewModel.cs
+++ CryptoCoinMon/CryptoCoinMon/ViewModels/QRViewModel.cs
@@ -55,6 +55,7 @@ namespace CryptoCoinMon.ViewModels
                         IsScanning = false;
                         ScannedCode = result.Text;
                         await _qRCodeGetterAPI.SendQRCode(ScannedCode);
+                        await Application.Current.MainPage.Navigation.PopAsync();
                     });
                 };
 
